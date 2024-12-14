2ba216f56b5597651aa877f0e3adb218239877ec
Alexander Vetrenko <vetrenkosasha@gmail.com>
2024-12-14 23:00:53 +0500

    Правка cors

---
+++ src/main/java/ru/rsc/clicker_kombat/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ru.rsc.clicker_kombat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+}
